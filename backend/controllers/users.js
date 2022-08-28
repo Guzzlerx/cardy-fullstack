8ab8427369b0a2fe76092c3827048c70456c30ca
Andrew <pasynkov2010@icloud.com>
2022-08-28 14:29:56 +0300

    change port to 3000

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -177,6 +177,7 @@ function login(req, res, next) {
               sameSite: 'none',
               secure: true,
             })
+            .status(200)
             .send({ message: 'Вы успешно авторизировались!' });
         })
         .catch(() => next(new ServerError()));
