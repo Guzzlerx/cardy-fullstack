8cd8e61178bcf0e815badb4e85df0c19c2aa683e
Andrew <pasynkov2010@icloud.com>
2022-08-28 18:58:40 +0300

    add crash test

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -14,6 +14,12 @@ const {
   getCurrentUser,
 } = require('../controllers/users');
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 router.post(
   '/signin',
   celebrate({
